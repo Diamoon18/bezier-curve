9ad8cae0f56fc336fc1126867f10700e31c2e9f6
Dianamoon18 <72127610+Diamoon18@users.noreply.github.com>
2021-03-23 19:15:48 +0100

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,36 @@
+package Lab3_K;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Application extends JFrame {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public final int WIDTH = 650, HEIGHT = 500;
+    
+    public Application() {
+    	initUI();
+    }
+
+    private void initUI() {
+
+        add(new Board());
+
+        setSize(WIDTH, HEIGHT);
+
+        setTitle("Di");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }    
+    
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+            Application ex = new Application();
+            ex.setVisible(true);
+        });
+    }
+}
